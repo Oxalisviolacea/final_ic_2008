8124f4d86143a4f677c5daec2f2c92c43f2cda29
Hanna Davis <hanna.e.davis@gmail.com>
2020-09-23 10:28:21 -0600

    Test it can find all the cookbook ingredients

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -7,10 +7,18 @@ require './lib/cook_book'
 
 class CookBookTest < Minitest::Test
   def setup
-    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
-    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    @pantry = Pantry.new
     @recipe1 = Recipe.new("Mac and Cheese")
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
     @recipe2 = Recipe.new("Cheese Burger")
+    @recipe2.add_ingredient(@ingredient1, 2)
+    @recipe2.add_ingredient(@ingredient3, 4)
+    @recipe2.add_ingredient(@ingredient4, 1)
     @cookbook = CookBook.new
   end
 
@@ -25,5 +33,18 @@ class CookBookTest < Minitest::Test
     @cookbook.add_recipe(@recipe2)
 
     assert_equal [@recipe1, @recipe2], @cookbook.recipes
+  end
+
+  def test_it_can_find_the_cookbook_ingredients
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    @recipe2.add_ingredient(@ingredient1, 2)
+    @recipe2.add_ingredient(@ingredient3, 4)
+    @recipe2.add_ingredient(@ingredient4, 1)
+
+    expected = ["Cheese", "Macaroni", "Ground Beef", "Bun"]
+    assert_equal expected, @cookbook.ingredients
   end
 end
