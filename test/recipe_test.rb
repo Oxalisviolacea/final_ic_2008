2b5f841d6348edd3762930fd8ae442f7481ac393
Hanna Davis <hanna.e.davis@gmail.com>
2020-09-23 09:29:40 -0600

    Test it has attributes

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -14,4 +14,8 @@ class RecipeTest < Minitest::Test
   def test_it_exists
     assert_instance_of Recipe, @recipe1
   end
+
+  def test_it_has_attributes
+    assert_equal "Mac and Cheese", @recipe1.name
+  end
 end
