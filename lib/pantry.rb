293bd0dfd0e960b6fb6eda41d6a496374c29b2c4
Hanna Davis <hanna.e.davis@gmail.com>
2020-09-23 09:24:46 -0600

    Create methods to restock and check the stock of an ingredient

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -3,4 +3,12 @@ class Pantry
   def initialize
     @stock = Hash.new(0)
   end
+
+  def restock(ingredient, quantity)
+    @stock[ingredient] += quantity
+  end
+
+  def stock_check(ingredient)
+    @stock[ingredient]
+  end
 end
