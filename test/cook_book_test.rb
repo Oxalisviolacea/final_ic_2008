0fd18141034755207937891dad08e638802ce9af
Hanna Davis <hanna.e.davis@gmail.com>
2020-09-23 09:54:37 -0600

    Test it can add a recipe

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -17,4 +17,13 @@ class CookBookTest < Minitest::Test
   def test_it_exists
     assert_instance_of CookBook, @cookbook
   end
+
+  def test_it_can_add_a_recipe
+    assert_equal [], @cookbook.recipes
+
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
+
+    assert_equal [@recipe1, @recipe2], @cookbook.recipes
+  end
 end
