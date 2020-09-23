1a90ecadfad4c9d98e96940445271b526474555e
Hanna Davis <hanna.e.davis@gmail.com>
2020-09-23 09:24:18 -0600

    Test it can restock an item

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -16,6 +16,17 @@ class PantryTest < Minitest::Test
 
   def test_the_stock_is_empty_initially
     assert_equal ({}), @pantry.stock
-    assert_equal 0, @pantry.stock_check(ingredient1)
+    assert_equal 0, @pantry.stock_check(@ingredient1)
+  end
+
+  def test_it_can_restock_an_item
+    @pantry.restock(@ingredient1, 5)
+    @pantry.restock(@ingredient1, 10)
+
+    assert_equal 15, @pantry.stock_check(@ingredient1)
+
+    @pantry.restock(@ingredient2, 7)
+
+    assert_equal 7, @pantry.stock_check(@ingredient2)
   end
 end
