9c3135e10fdf1b03df33f2e1c659dd3608222a89
Hanna Davis <hanna.e.davis@gmail.com>
2020-09-23 10:28:49 -0600

    Create method to find all cookbook ingredients

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -8,4 +8,14 @@ class CookBook
   def add_recipe(recipe)
     @recipes << recipe
   end
+
+  def ingredients
+    names = []
+    @recipes.each do |recipe|
+      recipe.ingredients_required.each do |ingredient|
+        names << ingredient[0].name
+      end
+    end
+    names.uniq
+  end
 end
